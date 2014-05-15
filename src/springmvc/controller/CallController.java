b92260fb159fac9399f4b70eb42d0b3f87441440
Daniil Ivanov <Daniil Ivanov daniil.ivanov@gmail.com>
2014-05-15 22:32:15 +0300

    Map JSON input to POJO automatically

--- src/springmvc/controller/CallController.java
+++ src/springmvc/controller/CallController.java
@@ -1,6 +1,6 @@
 package springmvc.controller;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -8,28 +8,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 import springmvc.model.Response;
+import springmvc.model.Request;
 
 @Controller
 public class CallController {
 	private static final Logger logger = LoggerFactory.getLogger(CallController.class);
 
-	@RequestMapping(value="/call", method={RequestMethod.GET}, produces="application/json")
-	public ResponseEntity<Response> get(HttpServletResponse resp) {
+	@RequestMapping(value="/call", method={RequestMethod.GET},
+		produces="application/json")
+	public ResponseEntity<Response> get(HttpServletRequest req) {
 		logger.info("call-get");
 		Response response = new Response("call-get");
 		return new ResponseEntity<Response>(
 			response, new HttpHeaders(), HttpStatus.OK);
 	}
 
-	@RequestMapping(value="/call", method={RequestMethod.POST}, produces="application/json")
-	public ResponseEntity<Response> post(HttpServletResponse resp) {
-		logger.info("call-post");
-		Response response = new Response("call-post");
+	@RequestMapping(value="/call", method={RequestMethod.POST},
+		consumes="application/json", produces="application/json")
+	public ResponseEntity<Response> post(@RequestBody Request req) {
+		logger.info("call-post {}", req.getInput());
+		Response response = new Response(req.getInput());
 		return new ResponseEntity<Response>(
 			response, new HttpHeaders(), HttpStatus.OK);
 	}
---
+++ src/springmvc/model/Request.java
@@ -0,0 +1,14 @@
+package springmvc.model;
+
+public class Request {
+
+	private String input;
+	
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
